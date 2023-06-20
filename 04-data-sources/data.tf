d09b305f11a198a1a1c2c90633ff45b1b9cde340
ram <ramthatigutla@gmail.com>
2023-06-20 09:42:38 +0530

    terraform datasource

---
+++ 04-data-sources/data.tf
@@ -0,0 +1,21 @@
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.medium"
+  availability_zone = "us-east-1a"
+
+  filter {
+    name   = "product-description"
+    values = ["Linux/UNIX"]
+  }
+}
+
+output "price" {
+  value = data.aws_ec2_spot_price.example.spot_price
+}
+
+data "aws_security_group" "selected" {
+  name = "allow-all"
+}
+
+output "sgid" {
+  value = data.aws_security_group.selected.id
+}
