97535ad486c79eef418eeb637574d85c99750c12
ram <ramthatigutla@gmail.com>
2023-06-20 11:45:31 +0530

    terraform provisioner

--- roboshop-v1/ec2/main.tf
+++ roboshop-v1/ec2/main.tf
@@ -2,17 +2,17 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
+
   tags = {
     Name = var.name
   }
-
   provisioner "remote-exec" {
 
     connection {
       type     = "ssh"
       user     = "centos"
       password = "DevOps321"
-      host     = aws_instance.web.public_ip
+      host     = self.public_ip
     }
 
     inline = [
