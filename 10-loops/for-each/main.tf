4e9964a965c28b2a601fb5a8e24c86650d112cd6
ram <ramthatigutla@gmail.com>
2023-06-28 11:45:09 +0530

    for each creation

---
+++ 10-loops/for-each/main.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "web" {
+  for_each      = var.instances
+  ami           = data.aws_ami.example.id
+  instance_type = "t3.small"
+
+  tags = {
+    Name = hi
+  }
+}
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = {
+
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.nano"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.nano"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.nano"
+    }
+
+  }
+}
